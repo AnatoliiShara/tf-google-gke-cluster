afc565905d4eda8534c50a45c4158401d34fa99d
AnatoliiShara <anfrankleen23@gmail.com>
2025-09-03 11:45:35 +0000

    Test: increase node count from 2 to 3 (fix: exclude .terraform)

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,22 @@
 output "config_host" {
-  value = "https://${data.google_container_cluster.main.endpoint}"
+  description = "GKE Cluster Host"
+  value       = data.google_container_cluster.main.endpoint
 }
 
 output "config_token" {
-  value = data.google_client_config.current.access_token
+  description = "GKE Cluster Token"
+  value       = data.google_client_config.current.access_token
+  sensitive   = true    # ← Має бути ВСЕРЕДИНІ блоку output!
 }
 
 output "config_ca" {
+  description = "GKE Cluster CA"
   value = base64decode(
     data.google_container_cluster.main.master_auth[0].cluster_ca_certificate,
   )
 }
 
 output "name" {
-  value = google_container_cluster.this.name
+  description = "GKE Cluster Name"
+  value       = google_container_cluster.this.name
 }
